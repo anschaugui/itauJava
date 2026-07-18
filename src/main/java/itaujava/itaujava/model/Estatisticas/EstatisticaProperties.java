c62f24bc294d472002cbf94d2497191ada4e66f8
anschaugui <anschau.gui@gmail.com>
2026-07-18 08:05:44 -0300

    add validation segundos.properties

--- src/main/java/itaujava/itaujava/model/Estatisticas/EstatisticaProperties.java
+++ src/main/java/itaujava/itaujava/model/Estatisticas/EstatisticaProperties.java
@@ -1,11 +1,16 @@
 package itaujava.itaujava.model.Estatisticas;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "estatistica")
 public record EstatisticaProperties(
+        // nao pode ser 0 ou numero negativo e tambem nao pode ser vazio
+    @NotNull(message = "Segundos nao aceita valores nulos")
     @Positive(message = "estatistica.segundos deve ser maior que zero")
     @Min(value = 1, message = "O tempo deve ser de no mínimo 1 segundo")
     Integer segundos
